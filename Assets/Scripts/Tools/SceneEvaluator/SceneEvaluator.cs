f6ded2f29d977bba41da166b2750e4f9e1aa90af
NicoSchoeman <9889916+nico-schoeman@users.noreply.github.com>
2021-06-14 15:54:53 +0200

    filter, display and select gameobjects that match criteria.

--- Assets/Scripts/Tools/SceneEvaluator/SceneEvaluator.cs
+++ Assets/Scripts/Tools/SceneEvaluator/SceneEvaluator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using System.Linq;
 
 #if (UNITY_EDITOR)
@@ -11,7 +12,10 @@ namespace Tools
     public class SceneEvaluator : EditorWindow
     {
         private int tabIndex = -1;
+        private CatagoryTabBase activeTab;
         private static SortedList<string, System.Type> tabs = new SortedList<string, System.Type>();
+
+        private Vector2 scroll = Vector2.zero;
 
         // Add menu named "SceneEvaluator" to the Window menu
         [MenuItem("24bit Tools/SceneEvaluator %e")] //Hotkey is Ctrl-E
@@ -27,7 +31,7 @@ namespace Tools
             FindTabs();
         }
 
-        static void FindTabs ()
+        static void FindTabs()
         {
             try
             {
@@ -39,7 +43,7 @@ namespace Tools
                     {
                         if (!tabs.ContainsKey(type.Name) && type.GetInterfaces().Contains(typeof(ICatagoryTab)) && type.BaseType.Equals(typeof(CatagoryTabBase)))
                         {
-                            tabs.Add(type.Name, type);
+                            tabs.Add(type.Name.Replace("Tab", ""), type);
                         }
                     }
                 }
@@ -58,10 +62,40 @@ namespace Tools
             tabIndex = GUILayout.SelectionGrid(tabIndex, tabs.Keys.ToArray<string>(), tabs.Count);
             if (EditorGUI.EndChangeCheck())
             {
-                CatagoryTabBase tab = System.Activator.CreateInstance(tabs.Values[tabIndex]) as CatagoryTabBase;
-                tab.Draw();
+                activeTab = System.Activator.CreateInstance(tabs.Values[tabIndex]) as CatagoryTabBase;
+            }
+
+            if (activeTab != null)
+            {
+                activeTab.Draw();
+
+                EditorGUILayout.BeginScrollView(scroll);
+                for (int i = 0; i < activeTab.criteriaMatches.Count; i++)
+                {
+                    ListEntry entry = activeTab.criteriaMatches[i];
+                    entry.foldout = EditorGUILayout.BeginFoldoutHeaderGroup(entry.foldout, $"{entry.gameObject.name} (errors: {entry.validationErrors.Count})");
+                    activeTab.criteriaMatches[i] = entry;
+                    if (entry.foldout)
+                    {
+                        if (GUILayout.Button("Select in scene hierarchy", EditorStyles.linkLabel))
+                        {
+                            EditorGUIUtility.PingObject(entry.gameObject);
+                            Selection.activeObject = entry.gameObject;
+                        }
+                        GUILayout.Label($"{string.Join("\n", entry.validationErrors)}");
+                    }
+                    EditorGUILayout.EndFoldoutHeaderGroup();
+                }
+                EditorGUILayout.EndScrollView();
             }
         }
+    }
+
+    public struct ListEntry
+    {
+        public GameObject gameObject;
+        public List<string> validationErrors;
+        public bool foldout;
     }
 
     public interface ICatagoryTab
@@ -70,22 +104,95 @@ namespace Tools
         void Draw();
     }
 
-    public abstract class CatagoryTabBase: ICatagoryTab
+    public abstract class CatagoryTabBase : ICatagoryTab
     {
-        string name;
-        public abstract void Scan();
-        public void Draw()
+        public string Name
         {
-            Debug.Log("test");
+            get { return this.GetType().Name.Replace("Tab", ""); }
+        }
+
+        public List<ListEntry> criteriaMatches = new List<ListEntry>();
+
+        public abstract void Scan();
+        public virtual void Draw()
+        {
+            EditorGUILayout.LabelField(Name, EditorStyles.boldLabel);
+            if (GUILayout.Button("Scan"))
+            {
+                Scan();
+                Selection.objects = criteriaMatches.Select<ListEntry, GameObject>(match => { return match.gameObject; }).ToArray();
+            }
         }
     }
 
-    public class MeshTab: CatagoryTabBase
+    public class MeshTab : CatagoryTabBase
     {
-        string name = "Mesh";
-        public override void Scan ()
-        {
+        private bool ignoreMeshRenderers;
+        private bool ignoreSkinnedMeshRenderers;
+        private bool checkMaterialCount;
+        private bool checkVertexCount;
+        private int vertexCountValue;
 
+        public override void Draw()
+        {
+            base.Draw();
+            ignoreMeshRenderers = EditorGUILayout.ToggleLeft("Ignore Mesh Renderers", ignoreMeshRenderers);
+            ignoreSkinnedMeshRenderers = EditorGUILayout.ToggleLeft("Ignore Skinned Mesh Renderers", ignoreSkinnedMeshRenderers);
+            checkVertexCount = EditorGUILayout.BeginToggleGroup("Vertex count", checkVertexCount);
+            vertexCountValue = EditorGUILayout.IntSlider(vertexCountValue, 0, int.MaxValue);
+            EditorGUILayout.EndToggleGroup();
+            checkMaterialCount = EditorGUILayout.ToggleLeft("Has more than one material", checkMaterialCount);
+        }
+
+        public override void Scan()
+        {
+            criteriaMatches = new List<ListEntry>();
+            UnityEngine.SceneManagement.Scene scene = EditorSceneManager.GetActiveScene();
+            GameObject[] gameObjects = scene.GetRootGameObjects();
+
+            foreach (GameObject gameObject in gameObjects)
+            {
+                if (!ignoreMeshRenderers)
+                {
+                    MeshRenderer[] meshRenderers = gameObject.GetComponentsInChildren<MeshRenderer>(true);
+                    foreach (MeshRenderer meshRenderer in meshRenderers)
+                    {
+                        List<string> errors = new List<string>();
+                        MeshFilter meshFilter = meshRenderer.gameObject.GetComponent<MeshFilter>();
+                        if (meshFilter == null) errors.Add($"No mesh filter on gameObject.");
+                        if (meshFilter.sharedMesh == null) errors.Add($"MeshFilter has no Mesh assigned.");
+                        else if (checkVertexCount && meshFilter.sharedMesh.vertexCount > vertexCountValue) errors.Add($"Vertex count of {meshFilter.sharedMesh.vertexCount} exceeds {vertexCountValue}");
+                        if (checkMaterialCount && meshRenderer.sharedMaterials.Length > 1) errors.Add($"MeshRenderer Has {meshRenderer.sharedMaterials.Length} materials");
+                        if (errors.Count > 0) AddCriteriaMatch(meshRenderer.gameObject, errors);
+                    }
+                }
+
+                if (!ignoreSkinnedMeshRenderers)
+                {
+                    SkinnedMeshRenderer[] skinnedMeshRenderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>(true);
+                    foreach (SkinnedMeshRenderer skinnedMeshRenderer in skinnedMeshRenderers)
+                    {
+                        List<string> errors = new List<string>();
+                        if (skinnedMeshRenderer.sharedMesh == null) errors.Add($"SkinnedMeshRenderer has no Mesh assigned.");
+                        else if (checkVertexCount && skinnedMeshRenderer.sharedMesh.vertexCount > vertexCountValue) errors.Add($"Vertex count of {skinnedMeshRenderer.sharedMesh.vertexCount} exceeds {vertexCountValue}");
+                        if (checkMaterialCount && skinnedMeshRenderer.sharedMaterials.Length > 1) errors.Add($"SkinnedMeshRenderer Has {skinnedMeshRenderer.sharedMaterials.Length} materials");
+                        if (errors.Count > 0) AddCriteriaMatch(skinnedMeshRenderer.gameObject, errors);
+                    }
+                }
+            }
+        }
+
+        private void AddCriteriaMatch(GameObject gameObject, List<string> errors)
+        {
+            if (criteriaMatches.Any(match => match.gameObject == gameObject))
+            {
+                ListEntry entry = criteriaMatches.Single(match => match.gameObject == gameObject);
+                entry.validationErrors = entry.validationErrors.Union(errors).ToList();
+            }
+            else
+            {
+                criteriaMatches.Add(new ListEntry() { gameObject = gameObject, validationErrors = errors });
+            }
         }
     }
 }

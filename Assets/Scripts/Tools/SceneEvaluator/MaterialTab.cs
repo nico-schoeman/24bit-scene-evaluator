9b374b6e087b5832acd7a618dd59e1a1bc5d4c2c
NicoSchoeman <9889916+nico-schoeman@users.noreply.github.com>
2021-06-15 09:42:16 +0200

    Material tab template

---
+++ Assets/Scripts/Tools/SceneEvaluator/MaterialTab.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+#if (UNITY_EDITOR)
+namespace Tools
+{
+    /// <summary>
+    /// This class defines the Material tab and it's checks as well as draws the tab specific GUI parts
+    /// </summary>
+    public class MaterialTab : CatagoryTabBase
+    {
+        // The user defined criteria options are stored here
+
+        public override void Draw()
+        {
+            base.Draw();
+            // Here we draw the GUI for the filter options
+            
+        }
+
+        public override void Scan()
+        {
+            criteriaMatches = new List<ListEntry>();
+
+            foreach (GameObject gameObject in GetGameObjects())
+            {
+            }
+        }
+    }
+}
+#endif
